97a665b7e969f0446ec9c2ddf9062629fb270cbd
Patrick Posner <hello@patrickposner.dev>
2025-08-29 11:19:01 -0400

    custom validation for HTML

--- src/blocks/mega-menu/render.php
+++ src/blocks/mega-menu/render.php
@@ -47,6 +47,28 @@ $menu_classes .= $justify_menu ? ' menu-justified-' . $justify_menu : '';
 // Icons.
 $close_icon  = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" aria-hidden="true" focusable="false"><path d="M13 11.8l6.1-6.3-1-1-6.1 6.2-6.1-6.2-1 1 6.1 6.3-6.5 6.7 1 1 6.5-6.6 6.5 6.6 1-1z"></path></svg>';
 $toggle_icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" width="12" height="12" aria-hidden="true" focusable="false" fill="none"><path d="M1.50002 4L6.00002 8L10.5 4" stroke-width="1.5"></path></svg>';
+
+// Allowed HTML for sanitized output in menu toggle and close button icons.
+$allowed_html = array(
+	'span' => array(
+		'class' => true,
+		'id' => true,
+		'aria-hidden' => true,
+	),
+	'svg' => array(
+		'xmlns' => true,
+		'viewBox' => true,
+		'width' => true,
+		'height' => true,
+		'aria-hidden' => true,
+		'focusable' => true,
+		'fill' => true,
+	),
+	'path' => array(
+		'd' => true,
+		'stroke-width' => true,
+	),
+);
 ?>
 
 <li
@@ -87,7 +109,9 @@ $toggle_icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" widt
 		aria-describedby="<?php echo esc_attr( $unique_id ); ?>-desc"
 		<?php endif; ?>
 	>
-		<?php echo $toggle_content; ?>
+		<?php
+		echo wp_kses( $toggle_content, $allowed_html );
+		?>
 	</<?php echo esc_html( $tag_name ); ?>>
 
 	<div
@@ -108,7 +132,9 @@ $toggle_icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" widt
 			data-wp-on--click="actions.closeMenuOnClick"
 			type="button"
 		>
-			<?php echo $close_icon; ?>
+			<?php
+			echo wp_kses( $close_icon, $allowed_html );
+			?>
 		</button>
 	</div>
 
