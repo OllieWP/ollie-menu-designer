0d9f8d8732c4944d646bbb60c084bb4f58918290
Patrick Posner <hello@patrickposner.dev>
2025-08-29 11:08:40 -0400

    dependency update

--- build/blocks/mega-menu/index.asset.php
+++ build/blocks/mega-menu/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '9bd91bf37cf1f9f609ef');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'bf8d8b6e4ec8a23ea9e3');
