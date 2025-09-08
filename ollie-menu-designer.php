a79011e73908dc779c947d9c10a0a321ecdeae82
Patrick Posner <hello@patrickposner.dev>
2025-09-08 14:55:04 -0400

    updated author

--- ollie-menu-designer.php
+++ ollie-menu-designer.php
@@ -5,7 +5,8 @@
  * Requires at least: 6.5
  * Requires PHP:      7.4
  * Version:           0.1.7
- * Author:            Mike McAlister
+ * Author:            OllieWP Team
+ * Author URI:        https://olliewp.com
  * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:       ollie-menu-designer
