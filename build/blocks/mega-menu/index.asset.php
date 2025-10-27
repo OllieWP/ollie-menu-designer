d2bed2113d4fc38203f89ef20ce573b54704bf35
Patrick Posner <hello@patrickposner.dev>
2025-10-27 16:22:43 -0600

    updated build

--- build/blocks/mega-menu/index.asset.php
+++ build/blocks/mega-menu/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '28a09c11c62a44981532');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'd2376834dff50fc5f4b0');
--- build/blocks/mega-menu/view.asset.php
+++ build/blocks/mega-menu/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '2ceabd2d25ebed641d01', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'b8ccb3d246d2b8598525', 'type' => 'module');
