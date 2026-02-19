34c6998a2bef4d6dd5cac19641b91fbe0a26e97f
Mike McAlister <m@mikemcalister.com>
2026-02-19 09:56:47 -0600

    Additional animation improvements

--- build/blocks/mega-menu/index.asset.php
+++ build/blocks/mega-menu/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'f23d0e7510f61f9fde94');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'c476f2db0690db7eddae');
