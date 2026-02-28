fb422da966d1c12846ebb10d6d13010f304c94ca
Mike McAlister <m@mikemcalister.com>
2026-02-27 22:30:36 -0600

    Readme update and build

--- build/blocks/mega-menu/index.asset.php
+++ build/blocks/mega-menu/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'c476f2db0690db7eddae');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '316568dd106d91bf23ce');
