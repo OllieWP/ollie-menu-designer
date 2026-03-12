9be3a77d91c095c1ff88064a6e19bead6bbbb4ad
Mike McAlister <m@mikemcalister.com>
2026-03-12 10:47:49 -0500

    Default top spacing to 40px for new mega menu blocks

--- build/blocks/mega-menu/index.asset.php
+++ build/blocks/mega-menu/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '5c94da8dadd5eb7aaf35');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '659cd4da98a5155f3673');
--- build/blocks/mega-menu/view.asset.php
+++ build/blocks/mega-menu/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '1995e56b4d695cf69f78', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'e63510312d833d2026d0', 'type' => 'module');
